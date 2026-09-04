3b085bf3dc272fecf95cfc0e8a3094b0cf10753c
Wei Wen <weiwenn.tee@gmail.com>
2026-09-04 15:18:08 +0800

    Second Post

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("====================")
 print("Welcome here")
 print("My first post!")
 print("====================")
+
+username = "Jane Doe"
+bio = "Vibe Coder!"
+followers = 1000
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
