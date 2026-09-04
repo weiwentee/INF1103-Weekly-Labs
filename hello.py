a99c2f5551c215f9f93e734c1561c15f7b52788c
Wei Wen <weiwenn.tee@gmail.com>
2026-09-04 15:31:23 +0800

    Third Post!

--- hello.py
+++ hello.py
@@ -5,8 +5,18 @@ print("====================")
 
 username = "Jane Doe"
 bio = "Vibe Coder!"
-followers = 1000
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
 
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
